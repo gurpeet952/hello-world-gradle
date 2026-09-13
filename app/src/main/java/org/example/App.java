14cbcb6dfe27945a1aa67b49024a2547b7bf2e44
gpoai952-cloud <gpoai952@gmail.com>
2026-09-12 23:44:54 -0400

    initial change

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello Metro College!";
+        return "Hello Gurpreet!";
     }
 
     public static void main(String[] args) {
